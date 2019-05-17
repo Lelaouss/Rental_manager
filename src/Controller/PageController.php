b1aae2f7d19c067cecdc2609f2daf79aef2b765d
Lelaouss <leonce.piron.pro@gmail.com>
2019-05-17 07:41:52 +0200

    Mise en place page LOCAUX

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -5,13 +5,31 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Class PageController
+ * @package App\Controller
+ */
 class PageController extends AbstractController
 {
 	/**
+	 * Fonction home
+	 * Permet l'accès à la page d'accueil
+	 * 
 	 * @return Response
 	 */
     public function home(): Response
     {
         return $this->render('pages/homepage.html.twig');
     }
+	
+	/**
+	 * Fonction properties
+	 * Permet l'accès à la page de gestion des locaux
+	 *
+	 * @return Response
+	 */
+	public function properties(): Response
+	{
+		return $this->render('pages/properties.html.twig');
+	}
 }
