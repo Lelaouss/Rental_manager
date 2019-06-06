df123dce2ff263da53a4dcf1d19caa8acc7a78b5
Lelaouss <leonce.piron@hotmail.fr>
2019-06-06 15:01:15 +0200

    Ajout authentification sur les tests fonctionnels

--- tests/Controller/PageControllerTest.php
+++ tests/Controller/PageControllerTest.php
@@ -6,12 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class PageControllerTest extends WebTestCase
 {
-    public function testHome()
+	private $_client = null;
+	
+	public function setUp()
+	{
+		$this->_client = static::createClient([], [
+			'PHP_AUTH_USER' => 'testeur',
+			'PHP_AUTH_PW'   => 'manager*',
+		]);
+	}
+	
+	public function testHome()
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/home');
+		$crawler = $this->_client->request('GET', '/home');
 
-        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $this->assertSame(200, $this->_client->getResponse()->getStatusCode());
         
         $this->assertContains('Rental Manager', $crawler->filter('h1')->text());
         $this->assertContains('Bienvenue', $crawler->filter('h2')->text());
@@ -19,22 +28,20 @@ class PageControllerTest extends WebTestCase
     
     public function testHomeLinkToHome()
 	{
-		$client = static::createClient();
-		$crawler = $client->request('GET', '/home');
+		$crawler = $this->_client->request('GET', '/home');
 		
 		$link = $crawler->filter('a:contains("Rental Manager")')->eq(0)->link();
-		$crawler = $client->click($link);
-		$this->assertSame(200, $client->getResponse()->getStatusCode());
+		$crawler = $this->_client->click($link);
+		$this->assertSame(200, $this->_client->getResponse()->getStatusCode());
 		$this->assertContains('Bienvenue', $crawler->filter('h2')->text());
 	}
 	
 	public function testPropertiesLink()
 	{
-		$client = static::createClient();
-		$client->request('GET', '/home');
-		$crawler = $client->clickLink('LOCAUX');
+		$this->_client->request('GET', '/home');
+		$crawler = $this->_client->clickLink('LOCAUX');
 		
-		$this->assertSame(200, $client->getResponse()->getStatusCode());
+		$this->assertSame(200, $this->_client->getResponse()->getStatusCode());
 		$this->assertContains('LOCAUX', $crawler->filter('h3')->text());
 	}
 }
